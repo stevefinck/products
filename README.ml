e77eae525d8d875b8b4697488ab4319899cb595e
Steve Finck <steve.finck@ideas.com>
2016-09-21 12:26:21 -0500

    add README

---
+++ README.ml
@@ -0,0 +1,19 @@
+Dependencies 
+============
+Java 1.8.0_40
+Gradle 2.13 (Groovy 2.4.4)
+MongoDB 3.0.0
+
+To Run Tests
+============
+gradle clean build to compile and run tests
+
+To Run The App
+==============
+gradle bootrun
+
+navigate to http://localhost:8080 in a browser or via curl
+
+http://localhost:8080/products/1 get a specific product
+http://localhost:8080/productPrices to get/put product prices
+
